53329b754e565d03b3a670de7c1d04b8d83fec34
mr_morrison <qiriagivi@gmail.com>
2023-04-27 17:56:25 +0400

    Initial commit

---
+++ app/src/main/java/com/example/timer_android_ug/GetRoom.kt
@@ -0,0 +1,41 @@
+package com.example.timer_android_ug
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class GetRoom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_room)
+        if (checkRoomId()){
+            val intent = Intent(this, TimeCounter::class.java)
+            startActivity(intent)
+        }else{
+            saveRoom()
+        }
+    }
+    private fun saveRoom(){
+        val button = findViewById<Button>(R.id.submitRoom)
+        val roomId = findViewById<EditText>(R.id.getRoom)
+        val sharedPreferences = getSharedPreferences("roomID_pref", Context.MODE_PRIVATE)
+
+
+        button.setOnClickListener {
+            sharedPreferences.edit().putString("roomID", roomId.text.toString()).apply()
+            val intent = Intent(this, TimeCounter::class.java)
+            startActivity(intent)
+        }
+    }
+    private  fun  checkRoomId():Boolean{
+        val sharedPreferences = getSharedPreferences("roomID_pref", Context.MODE_PRIVATE)
+        val text = sharedPreferences.getString("roomID", "")
+        return text!="";
+    }
+    override fun onBackPressed() {
+    }
+}
---
+++ app/src/main/java/com/example/timer_android_ug/TimeCounter.kt
@@ -0,0 +1,152 @@
+package com.example.timer_android_ug
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.NetworkInterface
+import java.net.SocketException
+import java.util.Collections
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+
+class TimeCounter : AppCompatActivity() {
+    private lateinit var timerTextView: TextView
+    private lateinit var ipAddress: TextView
+    private lateinit var roomId: TextView
+    private var server: NettyApplicationEngine? = null
+    private var timerRunning = false
+    private val port=8080
+    @OptIn(DelicateCoroutinesApi::class)
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_counter)
+        val sharedPreferences = getSharedPreferences("roomID_pref", Context.MODE_PRIVATE)
+        val room = sharedPreferences.getString("roomID", "")
+        ipAddress=findViewById(R.id.ipAddress)
+        roomId=findViewById(R.id.getRoom)
+        ipAddress.text=getLocalIPAddress(this)
+        roomId.text="RoomId : $room"
+        exit()
+        GlobalScope.launch(Dispatchers.IO) {
+            startWebServer()
+        }
+
+    }
+
+    private fun startWebServer() {
+        if (server == null) {
+            server = embeddedServer(Netty, port = port) {
+                routing {
+                    get("/{input}") {
+                        val input = call.parameters["input"] ?: ""
+
+                        if (input == "start" && !timerRunning) {
+                            timerRunning = true
+
+                            Thread {
+                                startTimer()
+                            }.start()
+                        }
+                        call.respondText("Received: $input", ContentType.Text.Plain)
+                    }
+                }
+            }
+            server!!.start(wait = false)
+        } else {
+            println("Server already running")
+        }
+    }
+    fun stopWebServer() {
+        if (server != null) {
+            server!!.stop(0, 0)
+            server = null
+        } else {
+            println("Server is not running")
+        }
+    }
+    private fun startTimer() {
+        runOnUiThread {
+            timerTextView = findViewById(R.id.timerTextView)
+            val twoHoursInMillis = 2 * 60 * 60 * 1000L
+            val countDownTimer = object : CountDownTimer(twoHoursInMillis, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val hours = TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
+                    val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60
+                    val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60
+
+                    timerTextView.text = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds)
+                }
+
+                override fun onFinish() {
+                    timerTextView.text = "00:00:00"
+                    timerRunning = false
+                }
+            }
+            countDownTimer.start()
+        }
+    }
+
+    fun getLocalIPAddress(context: Context): String? {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+
+        if (networkCapabilities != null) {
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                return getLocalWifiOrEthernetIPAddress()
+            }
+        }
+        return null
+    }
+
+    private fun getLocalWifiOrEthernetIPAddress(): String? {
+        try {
+            val networkInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (networkInterface in networkInterfaces) {
+                val inetAddresses = Collections.list(networkInterface.inetAddresses)
+                for (inetAddress in inetAddresses) {
+                    if (!inetAddress.isLoopbackAddress && inetAddress.isSiteLocalAddress) {
+                        return inetAddress.hostAddress
+                    }
+                }
+            }
+        } catch (ex: SocketException) {
+            ex.printStackTrace()
+        }
+        return null
+    }
+    override fun onBackPressed() {
+    }
+    private fun exit(){
+        val exit = findViewById<ImageButton>(R.id.exit)
+        val sharedPreferences = getSharedPreferences("roomID_pref", Context.MODE_PRIVATE)
+
+
+        exit.setOnClickListener {
+            stopWebServer()
+            sharedPreferences.edit().putString("roomID", "").apply()
+            val intent = Intent(this, GetRoom::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
